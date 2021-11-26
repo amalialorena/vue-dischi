3c70dd111e95884de971604e5d4aa3c019f05cba
Amalia <amalialorenaferariu@gmail.com>
2021-11-26 14:45:12 +0100

    cards, data from api

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -10,7 +10,7 @@
 
 <script>
 import Card from '@/components/Card.vue';
-// import axios from 'axios'
+import axios from 'axios'
 
 export default {
   name: 'CardsContainer',
@@ -19,37 +19,12 @@ export default {
    },
 data() {
     return {
-        // apiUrl: "https://flynn.boolean.careers/exercises/api/array/music",
-        disks: [
-        {
-            "poster": "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
-            "title": "New Jersey",
-            "author": "Bon Jovi",
-            "genre": "Rock",
-            "year": "1988"
-        },
-        {
-            "poster": "https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg",
-            "title": "Live at Wembley 86",
-            "author": "Queen",
-            "genre": "Pop",
-            "year": "1992"
-        }
-       
-    ]
+        apiUrl: "https://flynn.boolean.careers/exercises/api/array/music",
+        disks: []
     }
-}
-  
-};
+},
 
-
-
-
-
-</script>
-
-
-<!--created () {
+created () {
     this.getDisks()
 },
 
@@ -64,7 +39,12 @@ methods: {
 
     }
 }
-} -->
+}
+
+</script>
+
+
+<!-- -->
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
